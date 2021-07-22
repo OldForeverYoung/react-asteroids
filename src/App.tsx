9f6b3fa38d0158248c5c2339820272cb0f2db7dc
OldForeverYoung <alien100101@gmail.com>
2021-07-22 20:15:02 +0300

    add asteroids ads

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { Asteroid } from "./Asteroid";
-
+interface Coor {
+  x: number;
+  y: number;
+}
 function App() {
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(10);
+  const [coors, setCoors] = useState<Coor[]>([]);
+
   return (
     <div>
-      <Asteroid x={45} y={5} />
-      <Asteroid x={60} y={-20} />
-      <Asteroid x={-1} y={20} />
+      <label>
+        coor X:
+        <input
+          type="number"
+          value={x}
+          onChange={(event) => {
+            setX(event.target.valueAsNumber);
+          }}
+        />
+      </label>
+      <label>
+        coor Y:
+        <input
+          type="number"
+          value={y}
+          onChange={(event) => {
+            setY(event.target.valueAsNumber);
+          }}
+        />
+      </label>
+      <button
+        onClick={() => {
+          setCoors([...coors, { x: x, y: y }]);
+        }}
+      >
+        add
+      </button>
+
+      {coors.map((c) => (
+        <Asteroid x={c.x} y={c.y} />
+      ))}
     </div>
   );
 }
